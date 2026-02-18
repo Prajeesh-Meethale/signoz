57b426cad8ab5f64140d32ee5e4d1ee3725d492a
Karan Balani <29383381+balanikaran@users.noreply.github.com>
2026-02-18 19:41:48 +0530

    feat: add get deployments api also

--- frontend/src/api/generated/services/sigNoz.schemas.ts
+++ frontend/src/api/generated/services/sigNoz.schemas.ts
@@ -1561,6 +1561,151 @@ export interface TypesUserDTO {
 	updatedAt?: Date;
 }
 
+export interface ZeustypesClusterResponseModelDTO {
+	/**
+	 * @type string
+	 */
+	address?: string;
+	/**
+	 * @type integer
+	 * @format int64
+	 */
+	buffer?: number;
+	/**
+	 * @type string
+	 */
+	ca?: string;
+	/**
+	 * @type string
+	 */
+	cloud_account_id?: string;
+	/**
+	 * @type string
+	 */
+	cloud_provider?: string;
+	/**
+	 * @type string
+	 */
+	cloud_region?: string;
+	/**
+	 * @type string
+	 * @format date-time
+	 */
+	created_at?: Date;
+	/**
+	 * @type string
+	 */
+	id?: string;
+	/**
+	 * @type string
+	 */
+	name?: string;
+	region?: ZeustypesRegionModelDTO;
+	/**
+	 * @type string
+	 */
+	region_id?: string;
+	/**
+	 * @type string
+	 * @format date-time
+	 */
+	updated_at?: Date;
+}
+
+export interface ZeustypesDeploymentHistoryModelDTO {
+	/**
+	 * @type string
+	 * @format date-time
+	 */
+	created_at?: Date;
+	/**
+	 * @type string
+	 */
+	deployment_id?: string;
+	/**
+	 * @type string
+	 */
+	event?: string;
+	/**
+	 * @type string
+	 */
+	id?: string;
+	/**
+	 * @type string
+	 */
+	state_after?: string;
+	/**
+	 * @type string
+	 */
+	state_before?: string;
+}
+
+export interface ZeustypesGettableDeploymentDTO {
+	cluster?: ZeustypesClusterResponseModelDTO;
+	/**
+	 * @type string
+	 */
+	cluster_id?: string;
+	/**
+	 * @type string
+	 * @format date-time
+	 */
+	created_at?: Date;
+	/**
+	 * @type array
+	 * @nullable true
+	 */
+	histories?: ZeustypesDeploymentHistoryModelDTO[] | null;
+	/**
+	 * @type array
+	 */
+	hosts?: ZeustypesHostModelDTO[];
+	/**
+	 * @type string
+	 */
+	id?: string;
+	/**
+	 * @type string
+	 */
+	license_id?: string;
+	/**
+	 * @type string
+	 */
+	name?: string;
+	/**
+	 * @type string
+	 */
+	password?: string;
+	/**
+	 * @type string
+	 */
+	state?: string;
+	/**
+	 * @type string
+	 */
+	tier?: string;
+	/**
+	 * @type string
+	 * @format date-time
+	 */
+	updated_at?: Date;
+	/**
+	 * @type string
+	 */
+	user?: string;
+}
+
+export interface ZeustypesHostModelDTO {
+	/**
+	 * @type boolean
+	 */
+	is_default?: boolean;
+	/**
+	 * @type string
+	 */
+	name?: string;
+}
+
 export interface ZeustypesPostableHostDTO {
 	/**
 	 * @type string
@@ -1609,6 +1754,35 @@ export interface ZeustypesPostableProfileDTO {
 	 * @type string
 	 */
 	where_did_you_discover_signoz?: string;
+}
+
+export interface ZeustypesRegionModelDTO {
+	/**
+	 * @type string
+	 */
+	category?: string;
+	/**
+	 * @type string
+	 * @format date-time
+	 */
+	created_at?: Date;
+	/**
+	 * @type string
+	 */
+	dns?: string;
+	/**
+	 * @type string
+	 */
+	id?: string;
+	/**
+	 * @type string
+	 */
+	name?: string;
+	/**
+	 * @type string
+	 * @format date-time
+	 */
+	updated_at?: Date;
 }
 
 export type ChangePasswordPathParameters = {
@@ -2297,6 +2471,14 @@ export type CreateSessionByEmailPassword200 = {
 
 export type RotateSession200 = {
 	data?: AuthtypesGettableTokenDTO;
+	/**
+	 * @type string
+	 */
+	status?: string;
+};
+
+export type GetDeploymentsFromZeus200 = {
+	data?: ZeustypesGettableDeploymentDTO;
 	/**
 	 * @type string
 	 */
